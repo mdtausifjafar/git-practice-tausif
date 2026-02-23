20880df2cf8c9481ca89fd65a9ebbb0443969584
mdtausifjafar <mdtausifjafar@gmail.com>
2026-02-23 23:56:22 +0600

    Add basic calculator functions: add and subtract

--- src/main.py
+++ src/main.py
@@ -3,3 +3,12 @@ import datetime
 print("My name is: Md. Tausif Jafar")
 
 print("Today's date:", datetime.datetime.now().strftime("%d-%m-%Y"))
+
+
+# Call the functions from utils.py
+
+from utils import add, subtract
+
+print("Addition of 5 and 3:", add(5, 3))
+
+print("Subtraction of 5 and 3:", subtract(5, 3))
