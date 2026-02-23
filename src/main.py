ec734b0e5e38122386adb0e53c935a739f7862b9
mdtausifjafar <mdtausifjafar@gmail.com>
2026-02-24 00:39:43 +0600

    Added the new multiply function in main.py for testing

--- src/main.py
+++ src/main.py
@@ -7,8 +7,10 @@ print("Today's date:", datetime.datetime.now().strftime("%d-%m-%Y"))
 
 # Call the functions from utils.py
 
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Addition of 5 and 3:", add(5, 3))
 
 print("Subtraction of 5 and 3:", subtract(5, 3))
+
+print("Multiplication of 5 and 3:", multiply(5, 3))
